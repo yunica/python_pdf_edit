b24e3d635c76483a271f5ccbb1d2d417972daba1
junior <fmjg21@gmail.com>
2018-11-13 09:43:01 -0500

    poo en la ficha

--- segundo.py
+++ segundo.py
@@ -2,12 +2,14 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import legal
-from reportlab.lib.colors import black, red
+from reportlab.lib.colors import black, red, blue
 
 resultado_entrevista_choise = ['COMPLETA', 'RECHAZADA', 'AUSENTE']
 tipo_usuario_choise = ['ACTIVA', 'FACTIBLE', 'POTENCIAL', 'CLANDESTINA', ]
 
 tipo_interior_choise = ['DEPARTAMENTO', 'OFICINA', 'TIENDA', 'LOCAL', 'OTRO', 'OTRO']
+tipo_documento_identidad_choise = ["L.E / DNI", "RUC", "CARNET EXTRANJERIA.", "PASAPORTE", "P. NAC."]
+tipo_persona_entrevistada_choise = ['PROPIETARIO', 'INQUILINO', 'ENTIDAD PUBLICA/PRIVADA', 'FAMILIAR DIRECTO', 'OTRO']
 
 
 def ficha_num(txt='123456', canv=None):
@@ -43,8 +45,8 @@ class Punto():
 
 
 class Lista():
-    def __init__(self, choise=[], ubix=0, ubiy=0, separacion=0, lubix=[], lubiy=[], otro=False, separacionotro=0,
-                 otrotext=None):
+    def __init__(self, choise=[], ubix=0, ubiy=0, separacion=0, lubix=[], lubiy=[], text=None, otro=False,
+                 ubixotro=0, ubiyotro=0, otrotext=None):
         self.choise = choise
         self.ubix = ubix
         self.ubiy = ubiy
@@ -52,9 +54,11 @@ class Lista():
         self.lubix = lubix
         self.lubiy = lubiy
         self.otro = otro
-        self.separacionotro = separacionotro
+        self.ubixotro = ubixotro
+        self.ubiyotro = ubiyotro
         self.otrotext = otrotext
         self.listaobj = []
+        self.text = text
 
     def crear_bloque_horizontal(self):
         ubixsum = self.ubix
@@ -72,20 +76,25 @@ class Lista():
         except Exception:
             print(Exception.__str__())
 
-    def get_punto(self, txt=None):
-        if txt and self.listaobj:
-            for i in self.listaobj:
-                if i.txt == txt:
-                    return i
+    def get_punto(self):
+        if self.text and self.listaobj:
+            if self.text in self.choise:
+                for i in self.listaobj:
+                    if i.txt == self.text:
+                        return i
+            else:
+                return None
         else:
             return None
 
     def get_otro(self):
         if self.otro and self.otrotext:
-            opunto_otro = self.get_punto('OTRO')
-            if opunto_otro:
-                opunto = Punto(int(opunto_otro.ubix + self.separacionotro), opunto_otro.ubiy, self.otrotext)
-                return opunto
+            if 'OTRO' in self.choise and self.text == 'OTRO':
+                return Punto(self.ubixotro, self.ubiyotro, self.otrotext)
+            else:
+                return None
+        else:
+            return None
 
 
 packet = io.BytesIO()
@@ -99,44 +108,104 @@ can.drawString(124, 109, "X")
 # 1
 can = ficha_num(canv=can)
 # 2
-resultados_entrevista = Lista(resultado_entrevista_choise, 133, 713, 62)
-resultados_entrevista.crear_bloque_horizontal()
-oresultados_entrevista = resultados_entrevista.get_punto('AUSENTE')
+olist = Lista(choise=resultado_entrevista_choise, ubix=134, ubiy=714, separacion=62, text='AUSENTE')
+olist.crear_bloque_horizontal()
 can.setFillColor(red)
-can.drawString(oresultados_entrevista.ubix, oresultados_entrevista.ubiy, 'X')
+can.drawString(olist.get_punto().ubix, olist.get_punto().ubiy, 'X')
+olist = None
+opoint = None
 # 3
-resultados_entrevista = Lista(tipo_usuario_choise, 350, 713, 0, [352, 404, 444, 496])
-resultados_entrevista.crear_bloque_horizontal()
-oresultados_entrevista = resultados_entrevista.get_punto('CLANDESTINA')
-can.setFillColor(red)
-can.drawString(oresultados_entrevista.ubix, oresultados_entrevista.ubiy, 'X')
+olist = Lista(choise=tipo_usuario_choise, ubix=350, ubiy=714, lubix=[350, 400, 441, 492], text='CLANDESTINA')
+olist.crear_bloque_horizontal()
+can.drawString(olist.get_punto().ubix, olist.get_punto().ubiy, 'X')
 # 4
-can = texto_separado(ubix=130, ubiy=685,isseparado=False, canv=can)
-can = texto_separado(txt='3216544',ubix=500, ubiy=685,isseparado=False, canv=can)
-can = texto_separado(txt='HUAMANGA',ubix=490, ubiy=666,isseparado=False, canv=can)
-can = texto_separado(txt='flores martinez junior grover',ubix=80, ubiy=665,isseparado=False, canv=can)
-can = texto_separado(txt='Psje. mao tse tung 1081 MZA=A',ubix=80, ubiy=646,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=45, ubiy=618,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=100, ubiy=618,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=150, ubiy=618,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=200, ubiy=618,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=250, ubiy=618,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=310, ubiy=618,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=355, ubiy=618,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=415, ubiy=618,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=470, ubiy=618,isseparado=False, canv=can)
-can = texto_separado(txt='005',ubix=530, ubiy=618,isseparado=False, canv=can)
+can = texto_separado(ubix=130, ubiy=685, isseparado=False, canv=can)
+can = texto_separado(txt='3216544', ubix=500, ubiy=685, isseparado=False, canv=can)
+can = texto_separado(txt='HUAMANGA', ubix=490, ubiy=666, isseparado=False, canv=can)
+can = texto_separado(txt='flores martinez junior grover', ubix=80, ubiy=666, isseparado=False, canv=can)
+can = texto_separado(txt='Psje. mao tse tung 1081 MZA=A', ubix=80, ubiy=647, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=45, ubiy=620, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=100, ubiy=620, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=150, ubiy=620, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=200, ubiy=620, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=250, ubiy=620, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=310, ubiy=620, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=355, ubiy=620, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=415, ubiy=620, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=470, ubiy=620, isseparado=False, canv=can)
+can = texto_separado(txt='005', ubix=530, ubiy=620, isseparado=False, canv=can)
 # 5
+can = texto_separado(txt='1234', ubix=42, ubiy=571, isseparado=True, canv=can)
+can = texto_separado(txt='PSJE. MAO TSE TUNG 10801 MZA A', ubix=93, ubiy=571, txtsize=11, canv=can)
+can = texto_separado(txt='5678', ubix=42, ubiy=544, isseparado=True, canv=can)
+can = texto_separado(txt='PSJE. MAO TSE TUNG 10801 MZA AKJSH DKA JS HD', ubix=93, ubiy=544, txtsize=11, canv=can)
 
+can = texto_separado(txt='1234', ubix=45, ubiy=522, txtsize=10, isseparado=False, canv=can)
+can = texto_separado(txt='56789', ubix=95, ubiy=522, txtsize=10, isseparado=False, canv=can)
+can = texto_separado(txt='56789', ubix=142, ubiy=522, txtsize=10, isseparado=False, canv=can)
+can = texto_separado(txt='56789', ubix=175, ubiy=522, txtsize=10, isseparado=False, canv=can)
+can = texto_separado(txt='56789', ubix=205, ubiy=522, txtsize=10, isseparado=False, canv=can)
+can = texto_separado(txt='56789', ubix=235, ubiy=522, txtsize=10, isseparado=False, canv=can)
 
+olist = Lista(choise=tipo_interior_choise, ubiy=523, lubix=[309, 340, 381, 422, 462],
+              otro=True, ubixotro=490, ubiyotro=524, otrotext='asdasdad', text='OTRO')
+olist.crear_bloque_horizontal()
+can.setFillColor(red)
+can.drawString(olist.get_punto().ubix, olist.get_punto().ubiy, 'X')
+if olist.get_otro():
+    can.drawString(olist.get_otro().ubix, olist.get_otro().ubiy, olist.get_otro().txt)
+
+can = texto_separado(txt='asjdhas kasjhdkjashd kajshdkjash ', ubix=42, ubiy=502, txtsize=11, isseparado=False, canv=can)
+can = texto_separado(txt='tipo edi', ubix=423, ubiy=502, txtsize=11, isseparado=False, canv=can)
+can = texto_separado(txt='codigo edif ', ubix=485, ubiy=502, txtsize=11, isseparado=False, canv=can)
+can = texto_separado(txt='complemento complemente complemento complemente ', ubix=42, ubiy=484, txtsize=10,
+                     isseparado=False, canv=can)
+can = texto_separado(txt='sector muni ', ubix=95, ubiy=466, txtsize=11, isseparado=False, canv=can)
+can = texto_separado(txt='etapa  ', ubix=320, ubiy=466, txtsize=11, isseparado=False, canv=can)
+can = texto_separado(txt='zona zona ', ubix=475, ubiy=466, txtsize=11, isseparado=False, canv=can)
+# 6
+if False:
+    can = texto_separado(txt='X', ubix=145, ubiy=442, canv=can)
+else:
+    can = texto_separado(txt='X', ubix=52, ubiy=335, canv=can)
+
+can = texto_separado(txt='FLORES', ubix=45, ubiy=412, canv=can)
+can = texto_separado(txt='MARTINEZ', ubix=290, ubiy=412, canv=can)
+can = texto_separado(txt='JUNIOR GROVER', ubix=45, ubiy=386, canv=can)
+
+olist = Lista(choise=tipo_documento_identidad_choise, ubiy=359, lubix=[51, 83, 113, 166, 166], text='P. NAC.')
+olist.crear_bloque_horizontal()
+can.drawString(olist.get_punto().ubix, olist.get_punto().ubiy, 'X')
+if 'PASAPORTE' in ["PASAPORTE", "P. NAC."]:
+    can = texto_separado(txt='PASAPORTE', ubix=189, ubiy=360, txtsize=9, isseparado=False, canv=can)
+
+can = texto_separado(txt='70804143', ubix=248, ubiy=358, isseparado=False, canv=can)
+can = texto_separado(txt='327855', ubix=362, ubiy=358, isseparado=False, canv=can)
+can = texto_separado(txt='966 288 465', ubix=455, ubiy=358, isseparado=False, canv=can)
+can = texto_separado(txt='www .gooogle. com', ubix=140, ubiy=336, isseparado=False, canv=can)
+can = texto_separado(txt='FLORES', ubix=45, ubiy=309, canv=can)
+can = texto_separado(txt='MARTINEZ', ubix=220, ubiy=309, canv=can)
+can = texto_separado(txt='JUNIOR GROVER', ubix=370, ubiy=309, canv=can)
+can = texto_separado(txt='70804143', ubix=45, ubiy=288, txtsize=10, isseparado=False, canv=can)
+
+olist = Lista(choise=tipo_persona_entrevistada_choise, ubiy=287, lubix=[134, 187, 226, 310, 380], text='OTRO',
+              otro=True, ubixotro=400, ubiyotro=289, otrotext='asdasdasd')
+olist.crear_bloque_horizontal()
+can.drawString(olist.get_punto().ubix, olist.get_punto().ubiy, 'X')
+
+if olist.get_otro():
+    can = texto_separado(txt=olist.get_otro().txt, ubix=olist.get_otro().ubix, ubiy=olist.get_otro().ubiy, txtsize=9,
+                         isseparado=False, canv=can)
+
+can = texto_separado(txt='freesoftandskate@gmail.com', ubix=448, ubiy=290, txtsize=9, isseparado=False, canv=can)
 
 can.setFont("Helvetica", 5)
-can.setFillColor(red)
+can.setFillColor(blue)
 for i in range(0, 600, 10):
-    can.drawString(i, 500, str(i))
+    can.drawString(i, 455, str(i))
 
 for i in range(0, 840, 10):
-    can.drawString(500, i, str(i))
+    can.drawString(280, i, str(i))
 
 # segunda hoja
 can.showPage()
